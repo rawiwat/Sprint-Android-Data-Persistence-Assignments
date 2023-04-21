4c5357fadfa1ca0f1883de5baa309d552006c591
Peter <rawiwatpan@gmail.com>
2023-04-21 16:47:08 +0700

    M01 assignment commit 2

--- M01/Assignment/ReadingListRebuild/app/src/main/java/com/example/readinglistrebuild/EditBookActivity.kt
+++ M01/Assignment/ReadingListRebuild/app/src/main/java/com/example/readinglistrebuild/EditBookActivity.kt
@@ -2,10 +2,27 @@ package com.example.readinglistrebuild
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.EditText
 
 class EditBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_book)
+
+        val iD = getIntent().getStringExtra("id")
+        val selectedBook = getIntent().getStringExtra("Selected Book")
+
+        println(selectedBook)
+
+        val selectedBookProperty = selectedBook?.split(":")
+        val bookTitle = findViewById<EditText>(R.id.bookName)
+        val selectedBookTitle = Editable.Factory.getInstance().newEditable(selectedBookProperty?.get(0))
+        val bookReasonToRead = findViewById<EditText>(R.id.bookReasonToRead)
+        val selectedBookReasonToRead = Editable.Factory.getInstance().newEditable(selectedBookProperty?.get(1))
+
+        bookTitle.text = selectedBookTitle
+        bookReasonToRead.text = selectedBookReasonToRead
+
     }
 }
--- M01/Assignment/ReadingListRebuild/app/src/main/java/com/example/readinglistrebuild/MainActivity.kt
+++ M01/Assignment/ReadingListRebuild/app/src/main/java/com/example/readinglistrebuild/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.readinglistrebuild
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
@@ -15,14 +17,31 @@ class MainActivity : AppCompatActivity() {
         fun buildItemView(bookView : Book): TextView {
             val textView = TextView(this)
             textView.text = "${bookView.id} : ${bookView.title}"
+            textView.setOnClickListener{
+                val intent = Intent(this, EditBookActivity::class.java)
+                val bookProperty = textView.text
+                println(bookProperty)
+                intent.putExtra("Selected Book",bookProperty)
+                startActivity(intent)
+            }
+
             return textView
         }
+
+        val bookShelf = findViewById<LinearLayout>(R.id.bookScroll)
+
         val testBook = Book()
         //as the name suggest this is only for testing if the view work or not
         testBook.id = "1"
         testBook.title = "chocolate night"
 
-        findViewById<LinearLayout>(R.id.bookScroll).addView(buildItemView(testBook))
+        bookShelf.addView(buildItemView(testBook))
+
+        findViewById<Button>(R.id.addBookButton).setOnClickListener {
+            val intent = Intent(this, EditBookActivity::class.java)
+            intent.putExtra("id",bookShelf.childCount)
+            startActivity(intent)
+        }
 
     }
 }
